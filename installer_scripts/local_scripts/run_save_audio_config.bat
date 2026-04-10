8ae36a2428e16872acd8751e3669c7882abeecba
mefranklin6 <mefranklin@csuchico.edu>
2026-04-10 08:28:21 -0700

    elevate to admin if not already

--- installer_scripts/local_scripts/run_save_audio_config.bat
+++ installer_scripts/local_scripts/run_save_audio_config.bat
@@ -1,2 +1,10 @@
 @echo off
+setlocal EnableExtensions
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+	Powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+	exit /b
+)
+
 Powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File C:\ProgramData\CTS\save_audio_config.ps1
